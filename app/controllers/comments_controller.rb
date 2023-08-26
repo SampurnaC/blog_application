34e2bd5d219d9463658598642abe20f3af80901a
sam <sampurna.chapagainnn@gmail.com>
2023-08-26 13:36:12 +0100

    add user email and id to comments in views and controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
+    before_action :authenticate_user!
     
     def create
-
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
+        @comment.user_id=current_user.id
+        @comment.save!
         redirect_to @post
     end
 
