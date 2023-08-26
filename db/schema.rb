c4b463d048e7d30b209c3d3a589cdc50addba2d6
sam <sampurna.chapagainnn@gmail.com>
2023-08-26 13:05:39 +0100

    add relationship between user and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_26_110724) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_26_112032) do
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_26_110724) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
