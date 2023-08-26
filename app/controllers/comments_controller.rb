361a15141f5a584bc4c69dbeac53eff2e107737a
sam <sampurna.chapagainnn@gmail.com>
2023-08-26 10:37:02 +0100

    add posts and comments feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+    
+    def create
+
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params)
+        redirect_to @post
+    end
+
+    def destroy
+        @post = Post.find(params[:post_id])
+        @post.comments.find(params[:id]).destroy
+        redirect_to @post
+    end
+
+    private
+    
+    def comment_params
+        params.require(:comment).permit(:name, :comment)
+    end
+
+end
