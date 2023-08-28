72af7607884822790b60a80a7e2dc33824fcb354
sam <sampurna.chapagainnn@gmail.com>
2023-08-28 19:45:46 +0100

    add categories model,controller and relationship with posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_26_141514) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_182124) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
@@ -26,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_26_141514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "profiles", force: :cascade do |t|
