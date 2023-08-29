bc6d958ef723414b9324b5c56d2e6486c63dc4d1
sam <sampurna.chapagainnn@gmail.com>
2023-08-29 07:34:32 +0100

    add faker gem and add seed

--- Gemfile
+++ Gemfile
@@ -81,3 +81,4 @@ gem "pg", "~> 1.5"
 gem 'omniauth-github', '~> 2.0.0'
 gem "omniauth-rails_csrf_protection"
 gem "figaro"
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+User.create!(
+	email: "test@test.com",
+	password: "test@test.com",
+	password_confirmation: "test@test.com"
+)
+
+p "Created #{User.count} users"
+
+7.times do |index|
+	Category.create!(
+		name: Faker::Device.unique.manufacturer
+	)
+end
+p "Created #{Category.count} categories"
+
+10.times do |index|
+	Post.create!(
+		title: Faker::Quote.famous_last_words,
+		content: Faker::Quote.matz,
+		user: User.last,
+		category: Category.last
+	)
+end
+p "Created #{Post.count} posts"
+
+9.times do |index|
+	Comment.create!(
+		comment: Faker::Restaurant.description,
+		post_id: Post.all.map(&:id).sample,
+		user: User.last
+	)
+end
+p "Created #{Comment.count} comments"
