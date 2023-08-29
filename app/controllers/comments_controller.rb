3d06a2e3763fb7624dcd243dd5a2acd5c923875d
sam <sampurna.chapagainnn@gmail.com>
2023-08-29 07:34:52 +0100

    code to remove name field from comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,7 @@ class CommentsController < ApplicationController
     private
     
     def comment_params
-        params.require(:comment).permit(:name, :comment)
+        params.require(:comment).permit(:comment)
     end
 
 end
